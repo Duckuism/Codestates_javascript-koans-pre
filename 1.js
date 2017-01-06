38036382381fcd4322a247253d7d4c2646e913cd
한유덕 <Duck@han-yudeog-ui-MacBook-Pro.local>
2017-01-06 15:30:30 +0900

    test

---
+++ 1.js
@@ -0,0 +1,16 @@
+
+    var meglomaniac;
+
+    function 
+       meglomaniac() [ A:"Joker", B:"Harley"] ;
+    ;
+
+    it( function () {
+      console.log(meglomaniac.mastermind);
+    }); 
+
+    it( function () {
+      console.log(meglomaniac.henchwoman);
+      console.log(meglomaniac.henchWoman);
+    });
+  
